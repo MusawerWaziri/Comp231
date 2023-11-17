18ed9eecf4969ac5411a3a6dfeaac57e9e7451e3
grauconejo13 <vanessaf.victorino@yahoo.com>
2023-11-16 22:58:23 -0600

    NutriGuide Mobile Application template.

---
+++ app/src/main/java/com/example/nutriguide/FoodCategoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nutriguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_category)
+    }
+}
---
+++ app/src/main/java/com/example/nutriguide/FoodListingAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.nutriguide
+
+class FoodListingAdapter {
+}
