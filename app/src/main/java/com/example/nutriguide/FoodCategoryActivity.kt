7abbe910bd403ae772f71b1eb481918aa8cbd1bd
AlexTCode <alextoronto66@gmail.com>
2023-11-19 17:36:46 -0500

    Add team member to comment

--- app/src/main/java/com/example/nutriguide/FoodCategoryActivity.kt
+++ app/src/main/java/com/example/nutriguide/FoodCategoryActivity.kt
@@ -3,6 +3,11 @@ package com.example.nutriguide
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*Alex Tang – Developer
+Abdul Waziri – Developer
+Sara Heimbecker – Developer
+Vanessa Victorino - Agile Customer*/
+
 class FoodCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
