87e456ab940194f801b8af4a60e56290f5789dc1
grauconejo13 <vanessaf.victorino@yahoo.com>
2023-12-09 16:35:11 -0600

    HomeActivity - added buttons for About NutriGuide, Category,and Contact.

--- app/src/main/java/com/example/nutriguide/HomeActivity.kt
+++ app/src/main/java/com/example/nutriguide/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.example.nutriguide
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 
 
 /**
@@ -11,14 +13,39 @@ import android.widget.Button
  */
 class HomeActivity : AppCompatActivity() {
     private lateinit var categoryButton: Button
+    private lateinit var contactButton: Button
+    private lateinit var buttonToggle: Button
+    private lateinit var layoutInfo: LinearLayout
+
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-            categoryButton = findViewById(R.id.category_btn)
+            buttonToggle = findViewById(R.id.aboutBtnToggle)
+            layoutInfo = findViewById<LinearLayout>(R.id.layoutInfo)
+            categoryButton = findViewById(R.id.btnCategory)
+            contactButton = findViewById(R.id.btnContact)
+
+            buttonToggle.setOnClickListener {
+                if (layoutInfo.visibility == View.GONE) {
+                    layoutInfo.visibility = View.VISIBLE
+                    buttonToggle.text = "Hide Info"
+                } else {
+                    layoutInfo.visibility = View.GONE
+                    buttonToggle.text = "About NutriGuide"
+                }
+            }
+
             categoryButton.setOnClickListener {
                 val intent = Intent(this, FoodCategoryActivity::class.java)
                 startActivity(intent)
             }
+
+            contactButton.setOnClickListener {
+                val intent = Intent(this, ContactActivity::class.java)
+                startActivity(intent)
+            }
+
    }
+
 }
